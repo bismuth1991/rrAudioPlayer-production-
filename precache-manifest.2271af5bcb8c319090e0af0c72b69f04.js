234944727f43f5a906b8517d2694020f415427da
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-28 04:52:36 -0500

    Initial build

---
+++ precache-manifest.2271af5bcb8c319090e0af0c72b69f04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5abecd5477bb44616449",
+    "url": "/static/js/main.5abecd54.chunk.js"
+  },
+  {
+    "revision": "22128efffa9ee7a204cf",
+    "url": "/static/js/1.22128eff.chunk.js"
+  },
+  {
+    "revision": "5abecd5477bb44616449",
+    "url": "/static/css/main.6ba06c32.chunk.css"
+  },
+  {
+    "revision": "ec3af7d8db4dc651413761beec39cc50",
+    "url": "/index.html"
+  }
+];
